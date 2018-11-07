10cbc6263d588bab1f2436cd9f15f7c29fa1060b
Alex <climberfields@gmail.com>
2018-11-06 21:14:26 -0600

    edit Incident updated

---
+++ src/EditIncident/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Modal, Form, Button, Label, Header } from 'semantic-ui-react';
+
+
+const EditIncident = (props) => {
+  console.log(props)
+  return (
+    <Modal open={props.open}>
+      <Header>Edit Incident</Header>
+      <Modal.Content>
+        <Form onSubmit={props.closeAndEdit}>
+          <Label>
+            Edit Incident Title:
+          </Label>
+          <Form.Input type='text' name='title' value={props.IncidentToEdit.title} onChange={props.handleEditChange}/>
+          <Label>
+            Edit Incident Description:
+          </Label>
+          <Form.Input type='text' name='description' value={props.IncidentToEdit.description} onChange={props.handleEditChange}/>
+
+          <Modal.Actions>
+            <Button color='green' type='submit'>Edit Incident</Button>
+          </Modal.Actions>
+        </Form>
+      </Modal.Content>
+    </Modal>
+    )
+}
+
+export default EditIncident;
